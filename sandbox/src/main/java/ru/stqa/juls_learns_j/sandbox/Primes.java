e426ded6c5bb6e3ac6891548122927825e774570
Juliia Shirokova <j.chirokova@gmail.com>
2022-06-07 17:33:52 +0200

    4.2 added collections to sandbox

--- sandbox/src/main/java/ru/stqa/juls_learns_j/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/juls_learns_j/sandbox/Primes.java
@@ -21,10 +21,9 @@ public class Primes {
   }
   public static boolean isPrimeWhile(int n){
     int i=2;
-    while (i<n){
+    while (i<n && n% i !=0){
          i++;
     }
-
     return i==n;
   }
 
