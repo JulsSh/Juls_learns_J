498e2552b19164c0fd341c326b318d141ab1a64f
Juliia Shirokova <j.chirokova@gmail.com>
2022-06-14 16:11:38 +0200

    4.8 added search for max element in collection

--- addressbook-web-tests/src/test/java/ru/stqa/juls_learns_j/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/juls_learns_j/addressbook/model/GroupData.java
@@ -3,12 +3,12 @@ package ru.stqa.juls_learns_j.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  private  int id;
   public  String groupName;
   public  String groupHeader;
   public  String groupFooter;
 
-  public GroupData(String id, String groupName, String groupHeader, String groupFooter) {
+  public GroupData(int id, String groupName, String groupHeader, String groupFooter) {
     this.groupName = groupName;
     this.groupHeader = groupHeader;
     this.groupFooter = groupFooter;
@@ -18,10 +18,14 @@ public class GroupData {
     this.groupName = groupName;
     this.groupHeader = groupHeader;
     this.groupFooter = groupFooter;
-    this.id = null;
+    this.id = 0;
   }
 
-  public String getId() {
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
     return id;
   }
 
@@ -38,6 +42,19 @@ public class GroupData {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(groupName, groupData.groupName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, groupName);
+  }
+
+  @Override
   public String toString() {
     return "GroupData{" +
             "id='" + id + '\'' +
@@ -45,16 +62,4 @@ public class GroupData {
             '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(groupName, groupData.groupName);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, groupName);
-  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/juls_learns_j/addressbook/tests/CreateGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/juls_learns_j/addressbook/tests/CreateGroupTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.*;
 
 import ru.stqa.juls_learns_j.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class CreateGroupTest extends TestBase {
@@ -13,15 +14,24 @@ public class CreateGroupTest extends TestBase {
 
     app.getNavigationManager().gotoGroupPage();
     List<GroupData> before=app.getGroupHelper().getGroupList();
-
+    GroupData group =new GroupData("juli2", null, null);
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupInfo(new GroupData("juli2", null, null));
+    app.getGroupHelper().fillGroupInfo(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after=app.getGroupHelper().getGroupList();
-
     Assert.assertEquals(after.size(), before.size() +1);
 
+    int max=0;
+    for (GroupData g :after){
+      if (g.getId() > max) {
+        max=g.getId();
+        group.setId(max);
+        before.add(group);
+
+      }
+    }
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }
