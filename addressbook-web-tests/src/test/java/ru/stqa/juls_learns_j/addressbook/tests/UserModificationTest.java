755cb62fb593c97aa0f04dec6564676f321b50b2
Juliia Shirokova <j.chirokova@gmail.com>
2022-06-22 15:31:38 +0200

    Update UserModificationTest.java

--- addressbook-web-tests/src/test/java/ru/stqa/juls_learns_j/addressbook/tests/UserModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/juls_learns_j/addressbook/tests/UserModificationTest.java
@@ -16,7 +16,9 @@ public class UserModificationTest extends TestBase{
 app.getNavigationManager().goToHomePage();
 app.getUserHelper().selectUser();
     app.getUserHelper().editSelectedUser();
-    app.getUserHelper().fillUserDetails(new UserData("222edited edit2", "222juls jennifer edit", "Birkin", "edit", "jiliianedit", "Zatoo", "alexanderplatz", "0123456", "0123456", "17910286119", null), false);
+    app.getUserHelper().fillUserDetails(new UserData("222edited edit2", "222juls jennifer edit",
+            "Birkin", "edit", "jiliianedit", "Zatoo", "alexanderplatz",
+            "0123456", "0123456", "17910286119", null), false);
     app.getUserHelper().submitUserModification();
 
 
